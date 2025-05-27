b57bbf7923b36ad43df0ba9629f23a6822bcf9f4
bigstudio106 <bigstudiomrjaat@gmail.com>
2025-05-27 13:12:41 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -14,14 +14,14 @@ class Config(object):
 	STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "NoNeed")
 	STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", "NoNeed")
 	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", True)
 	DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
 	PRESET = os.environ.get("PRESET", "ultrafast")
-	OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
-	CAPTION = "By @AHToolsBot"
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "VideoWatermark_Bot")
+	OWNER_ID = int(os.environ.get("OWNER_ID", ))
+	CAPTION = "By @MOVIE_INSIDER1"
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "VideoWatermark_Robot")
 	DATABASE_URL = os.environ.get("DATABASE_URL")
-	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
 	ALLOW_UPLOAD_TO_STREAMTAPE = bool(os.environ.get("ALLOW_UPLOAD_TO_STREAMTAPE", False))
 	USAGE_WATERMARK_ADDER = """
 Hi, I am Video Watermark Adder Bot!
@@ -31,7 +31,7 @@ Hi, I am Video Watermark Adder Bot!
 
 __Note: I can only process one video at a time. As my server is Heroku, my health is not good. If you have any issues with Adding Watermark to a Video, then please Report at [Support Group](https://t.me/linux_repo).__
 
-Desgined by @AbirHasan2005
+Desgined by @MOVIE_INSIDER1
 """
 	PROGRESS = """
 Percentage : {0}%
